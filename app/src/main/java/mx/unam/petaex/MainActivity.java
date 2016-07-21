152ce5a10d9d95056b4bc7c19535f64e687e914f
Herman Morales Ara <hmoralesara@hotmail.com>
2016-07-21 00:02:06 -0500

    Subo mi proyecto Petaex por primera vez al github

---
+++ app/src/main/java/mx/unam/petaex/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.unam.petaex;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
